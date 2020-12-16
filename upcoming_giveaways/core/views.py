36062c4239d1ea8b703faee04f93ce582da7269b
Parth <pdjani91@gmail.com>
2020-12-17 01:11:54 +0530

    1st & 2nd Module is done

--- upcoming_giveaways/core/views.py
+++ upcoming_giveaways/core/views.py
@@ -23,21 +23,6 @@ def signup(request):
 		form = UserCreationForm
 	return render(request,'registration/signup.html',{'form':form})
 
-@login_required
-def secret_page(request):
-	return render(request, 'secret_page.html')
-
-class SecretPage(LoginRequiredMixin, TemplateView):
-	template_name = 'secret_page.html'
-
-def upload(request):
-	context = {}
-	if request.method == 'POST':
-		uploaded_file = request.FILES['document']
-		fs = FileSystemStorage()
-		name = fs.save(uploaded_file.name, uploaded_file)
-		context['url'] = fs.url(name)
-	return render(request, 'upload.html',context)
 
 def book_list(request):
 	books = Book.objects.all()
@@ -56,16 +41,6 @@ def upload_book(request):
 		'form' : form
 		})
 
-class BookListView(ListView): #Generic Class Based Views
-	model = Book
-	template_name = 'class_book_list.html'
-	context_object_name = 'books'
-
-class UploadBookView(CreateView):
-	model = Book
-	form_class = BookForm
-	success_url = reverse_lazy('class_book_list')
-	template_name = 'upload_book.html'
 
 def delete_book(request,pk): # pk = primary key
 	if request.method == 'POST':
--- upcoming_giveaways/urls.py
+++ upcoming_giveaways/urls.py
@@ -7,13 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
 	path('',views.home, name='home'),
 	path('signup/',views.signup, name='signup'),
-	path('secret/',views.secret_page,name='secret'),
-	path('secret2/', views.SecretPage.as_view(),name='secret2'),
-	path('upload/', views.upload, name='upload'),
 	path('books/',views.book_list,name='book_list'),
     path('books/upload/',views.upload_book,name='upload_book'),
-    path('class/books/',views.BookListView.as_view(),name ='class_book_list'),
-    path('class/books/upload/',views.UploadBookView.as_view(),name ='class_upload_book'),
     path('books/<int:pk>/',views.delete_book,name='delete_book'),
 	path('accounts/',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
